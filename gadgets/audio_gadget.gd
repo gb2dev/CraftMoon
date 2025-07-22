4628783a7b9c3e17438741580d7781aab59c1ec5
Björn Feber <bfeber@protonmail.com>
2025-07-22 18:31:55 +0200

    Check unconnected_means_powered cases

--- gadgets/audio_gadget.gd
+++ gadgets/audio_gadget.gd
@@ -8,7 +8,7 @@ var is_pulse: bool
 func start() -> void:
 	change_property(&"ThreeD", false)
 	var _error := input_pulse.connect(func(_input_index: int) -> void:
-		if is_input_data_powered(0, false):
+		if is_input_data_powered(0, true):
 			is_pulse = false
 			audio_player.play()
 			check_pulse.call_deferred()
--- gadgets/look_at_rotator_gadget.gd
+++ gadgets/look_at_rotator_gadget.gd
@@ -6,6 +6,7 @@ func start() -> void:
 
 
 func tick(_delta: float) -> void:
+	# TODO: change to true when there is a pause/play system
 	if is_input_data_powered(0, false):
 		node_3d.get_parent_node_3d().look_at(get_viewport().get_camera_3d().global_position)
 
