65d41c08ab71fafd8c753daea2091573f16a68f9
Björn Feber <bfeber@protonmail.com>
2025-07-22 10:37:00 +0200

    Add counter icons

--- input_control.gd
+++ input_control.gd
@@ -1,5 +1,20 @@
+@tool
 class_name InputControl
 extends ColorRect
 
+@export var icon: Texture2D = preload("res://icons/power.svg"):
+	set(value):
+		icon = value
+		update_icon()
+
 var output_controls: Array[OutputControl]
 var output_visuals: Array[OutputVisual]
+
+@onready var texture_rect := $TextureRect as TextureRect
+
+func _ready() -> void:
+	update_icon()
+
+func update_icon() -> void:
+	if texture_rect:
+		texture_rect.texture = icon
