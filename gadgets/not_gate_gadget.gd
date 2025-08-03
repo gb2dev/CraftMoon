e01a81d80fc7cd5eacab86db44ef4c5ef6056173
Björn Feber <bfeber@protonmail.com>
2025-08-03 10:14:27 +0200

    Fix timer bugs

--- gadgets/not_gate_gadget.gd
+++ gadgets/not_gate_gadget.gd
@@ -4,8 +4,7 @@ extends Gadget
 func start() -> void:
 	output(0, true)
 	var _error := input_pulse.connect(func(_input_index: int) -> void:
-		if not World.time_paused:
-			output(0, not is_input_data_powered(0, false))
+		output(0, not is_input_data_powered(0, false))
 	)
 
 
